160e604c32ee668fbf67da3262c7e9b6e292623d
Hr810004 <hr810004@gmail.com>
2025-07-14 12:14:28 +0530

    Refactor main application structure to include AuthProvider
    - Wrapped the main application in AuthProvider to manage authentication context.
    - Removed AuthProvider from AppLayout to streamline component hierarchy and avoid redundancy.

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -6,14 +6,17 @@ import "./index.css";
 import App from "./App.tsx";
 import QueryProvider from "./context/query-provider.tsx";
 import { Toaster } from "./components/ui/toaster.tsx";
+import { AuthProvider } from "./context/auth-provider";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryProvider>
-      <NuqsAdapter>
-        <App />
-      </NuqsAdapter>
-      <Toaster />
+      <AuthProvider>
+        <NuqsAdapter>
+          <App />
+        </NuqsAdapter>
+        <Toaster />
+      </AuthProvider>
     </QueryProvider>
   </StrictMode>
 );
